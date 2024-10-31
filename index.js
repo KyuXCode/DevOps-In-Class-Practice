87a445601f4040e54f76832026885ecc0e71776b
Kevin Yu <kai38547@gmail.com>
2024-10-31 14:29:50 +0000

    Handle negative

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.factorial = function factorial(n) {
-    if (n < 0) return "Can't be negative"; 
+    if (n < 0) return undefined; 
     if (n === 0 || n === 1) return 1;
     return n * factorial(n - 1);
 }
